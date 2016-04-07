7dc5a9b110554e29a5b9c6a536488c8a14188d37
kiandr <davoudi.rk@gmail.com>
2016-04-06 17:03:32 -0700

    this is the sample code that I just commited for the insertion sorrt

---
+++ sampleCodes/InsertionSort/InsertionSort/InsertionSort/main.cpp
@@ -0,0 +1,25 @@
+//
+//  main.cpp
+//  InsertionSort
+//
+//  Created by Kian Davoudi-Rad on 2016-04-06.
+//  Copyright © 2016 Kian Davoudi. All rights reserved.
+//
+
+#include <iostream>
+#include "InsertionSort.h"
+int main(int argc, const char * argv[]) {
+    
+    // Please reffer to https://drive.google.com/folderview?id=0B1kNDgdfL1swclNicUgySnhLMXM&usp=sharing for theory.
+    // Step 1: What the data structure.
+    
+    ISNode *head = (ISNode*)malloc(sizeof(ISNode));
+    head->Key =0;
+    
+    
+    std::cout<<head->Key;
+    
+    
+    
+    return 0;
+}
