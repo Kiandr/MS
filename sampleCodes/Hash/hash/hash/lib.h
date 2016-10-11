8c187441dc97a6f34383e80fe143bfcc8f1bebb6
Kian Davoudi <kdavoudi@NurseNextDoor.local>
2016-10-11 12:58:03 -0700

    added some stuff

---
+++ sampleCodes/Hash/hash/hash/lib.h
@@ -0,0 +1,17 @@
+// basic opeartions of the hash table
+
+
+typedef struct HashNode {
+
+	char val[2];
+	int *hashKey;
+}node;
+
+class serviceHash {
+
+public:
+	void hashInsertNode() {};
+	void hashSearchNode() {};
+	void hashDeleteNode() {};
+	void hashPrintNode() {};
+};
---
+++ sampleCodes/Hash/hash/hash/main.cpp
@@ -0,0 +1,9 @@
+#include "lib.h"
+#include "iostream"
+
+void main() {
+
+	serviceHash *serviceCallHash = (serviceHash*)malloc(sizeof(serviceHash));
+
+
+};
