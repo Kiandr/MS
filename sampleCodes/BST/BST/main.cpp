9667d2f86d399947acba0f50406a8ec26356a7d7
Kian Davoudi-Rad <kiandavoudi-rad@Kians-MacBook-Pro.local>
2016-12-21 17:01:20 -0800

    deleted all of the codes that no longer need

--- sampleCodes/BST/BST/main.cpp
+++ sampleCodes/BST/BST/main.cpp
@@ -7,21 +7,21 @@ int main() {
 
 	
 	node *test = NULL;
-	node *testedHeader  = InitHeader(test,100);
-	node *testedInsertToRight = Insert(testedHeader, 160);
-	node *testedInsertToLeft = Insert(testedHeader, 30);
-	node *testedInsertTo45 = Insert(testedHeader, 45);
-	node *testedInsertTo15 = Insert(testedHeader, 15);
-	node *testedInsertTo130 = Insert(testedHeader, 130);
-	node *testedInsertTo210 = Insert(testedHeader, 210);
-    node *testedInsertTo310 = Insert(testedHeader, 310);
+    node *testedHeader  = InitHeader(test,120);
+//	node *testedInsertToRight = Insert(testedHeader, 180);
+//	node *testedInsertToLeft = Insert(testedHeader, 30);
+//	node *testedInsertTo45 = Insert(testedHeader, 45);
+//	node *testedInsertTo15 = Insert(testedHeader, 15);
+//	node *testedInsertTo130 = Insert(testedHeader, 130);
+//	node *testedInsertTo210 = Insert(testedHeader, 210);
+//    node *testedInsertTo310 = Insert(testedHeader, 310);
 
-	node *testerNode = BuildNewNode(15);
+//	node *testerNode = BuildNewNode(15);
 
-	node *tesSearch = Search(testedHeader, 15);
+//	node *tesSearch = Search(testedHeader, 15);
 
 
-    printf("test\n");
+    printf("End of the function !! \n");
 
 
 
