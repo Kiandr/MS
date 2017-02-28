c519d151162b56541dfc3ada83372f3e9ffbd17d
Kian Davoudi-Rad <kdr@Kians-MacBook-Pro.local>
2017-02-28 10:04:36 -0800

    adding first testing code for fabenacci. I learnt how to test a code and how to write a clean code

---
+++ sampleCodes/Fabenacci/fNumber.h
@@ -0,0 +1,19 @@
+#include "stdio.h"
+
+int getFabenacciNumber (int n){
+	int fNumber = 0;
+	
+	if (!n)
+		fNumber = 0;
+	else if (n==1)
+		fNumber = 1;
+	
+	else 
+	{
+		
+		
+	}
+
+return fNumber;	
+
+}
---
+++ sampleCodes/Fabenacci/main.c
@@ -0,0 +1,10 @@
+#include "stdio.h"
+#include "fNumber.h"
+
+int main(){
+	
+	int respond = getFabenacciNumber(0);
+	printf("this is the respond [%d] \n", respond);
+	
+	return 0;
+}
