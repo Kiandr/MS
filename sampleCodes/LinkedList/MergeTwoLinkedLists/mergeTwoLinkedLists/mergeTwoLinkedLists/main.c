c4f7e5e3d3f4a61c7af1a341c74a3692635483df
Kian Davoudi-Rad <davoudi.rk@gmail.com>
2017-08-14 10:23:04 -0700

    adding inital commit to the file

---
+++ sampleCodes/LinkedList/MergeTwoLinkedLists/mergeTwoLinkedLists/mergeTwoLinkedLists/main.c
@@ -0,0 +1,36 @@
+//
+//  main.c
+//  mergeTwoLinkedLists
+//
+//  Created by Kian Davoudi-Rad on 2017-08-14.
+//  Copyright © 2017 Kian Davoudi-Rad. All rights reserved.
+//
+
+#include <stdio.h>
+#include "merge.h"
+int main(int argc, const char * argv[]) {
+    // insert code here...
+
+
+    node * HeaderA = initHeaderWithEmptyModel();
+    node * HeaderB = initHeaderWithEmptyModel();
+
+    
+    // build two lists:
+    AddNewNodeWithMode (HeaderA, 50);
+    AddNewNodeWithMode (HeaderB, 51);
+    AddNewNodeWithMode (HeaderA, 52);
+    AddNewNodeWithMode (HeaderB, 53);
+    AddNewNodeWithMode (HeaderA, 54);
+    AddNewNodeWithMode (HeaderB, 55);
+    
+
+    // Print A and B
+    printList(HeaderA);
+    printList(HeaderB);
+    
+
+
+    printf("Hello, World!\n");
+    return 0;
+}
---
+++ sampleCodes/LinkedList/MergeTwoLinkedLists/mergeTwoLinkedLists/mergeTwoLinkedLists/merge.h
@@ -0,0 +1,28 @@
+//
+//  merge.h
+//  mergeTwoLinkedLists
+//
+//  Created by Kian Davoudi-Rad on 2017-08-14.
+//  Copyright © 2017 Kian Davoudi-Rad. All rights reserved.
+//
+
+#ifndef merge_h
+#define merge_h
+
+#include <stdio.h>
+
+
+typedef struct {
+    int val;
+    struct node * next;
+
+}node;
+
+node *initHeaderWithEmptyModel (void);
+int  AddNewNodeWithMode (node *header, int val);
+int  ifHeaderIsEmpty (node * model);
+void printList(node *header);
+node *findMiddleNodeInLinkedList (node *header);
+
+
+#endif /* merge_h */
