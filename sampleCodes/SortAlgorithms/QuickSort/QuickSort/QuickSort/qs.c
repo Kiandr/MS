77cd28bd12fe25c847ef177fe0a4157cd483cb8d
Kian Davoudi-Rad <davoudi.rk@gmail.com>
2017-08-18 12:32:28 -0700

    adding QuickSort project Empty Project

---
+++ sampleCodes/SortAlgorithms/QuickSort/QuickSort/QuickSort/qs.c
@@ -0,0 +1,9 @@
+//
+//  qs.c
+//  QuickSort
+//
+//  Created by Kian Davoudi-Rad on 2017-08-18.
+//  Copyright © 2017 Kian Davoudi-Rad. All rights reserved.
+//
+
+#include "qs.h"
---
+++ sampleCodes/SortAlgorithms/QuickSort/QuickSort/QuickSort/qs.h
@@ -0,0 +1,14 @@
+//
+//  qs.h
+//  QuickSort
+//
+//  Created by Kian Davoudi-Rad on 2017-08-18.
+//  Copyright © 2017 Kian Davoudi-Rad. All rights reserved.
+//
+
+#ifndef qs_h
+#define qs_h
+
+#include <stdio.h>
+
+#endif /* qs_h */
