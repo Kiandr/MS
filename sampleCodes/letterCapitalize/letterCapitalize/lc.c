97486c0723210ffabaf55a719f528fbbe8ffb309
Kian Davoudi-Rad <davoudi.rk@gmail.com>
2017-09-11 09:37:29 -0700

    adding letter capitalized

---
+++ sampleCodes/letterCapitalize/letterCapitalize/lc.c
@@ -0,0 +1,13 @@
+//
+//  lc.c
+//  letterCapitalize
+//
+//  Created by Kian Davoudi-Rad on 2017-09-11.
+//  Copyright © 2017 Kian Davoudi-Rad. All rights reserved.
+//
+#include "stdio.h"
+#include "lc.h"
+
+void letterCapitalized (char str[]){
+
+}
---
+++ sampleCodes/letterCapitalize/letterCapitalize/main.c
@@ -0,0 +1,16 @@
+//
+//  main.c
+//  letterCapitalize
+//
+//  Created by Kian Davoudi-Rad on 2017-09-11.
+//  Copyright © 2017 Kian Davoudi-Rad. All rights reserved.
+//
+
+#include <stdio.h>
+#include "lc.h"
+
+int main(int argc, const char * argv[]) {
+
+    letterCapitalized("This is the argument");
+    return 0;
+}
