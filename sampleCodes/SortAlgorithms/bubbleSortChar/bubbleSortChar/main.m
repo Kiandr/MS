4e66c68ddb404cb191bfd0f2054ca55b326a4dd6
Kian Davoudi-Rad <davoudi.rk@gmail.com>
2017-09-22 10:38:22 -0700

    adding bubble sort

--- sampleCodes/SortAlgorithms/bubbleSortChar/bubbleSortChar/main.m
+++ sampleCodes/SortAlgorithms/bubbleSortChar/bubbleSortChar/main.m
@@ -1,17 +1,51 @@
-//
-//  main.m
-//  bubbleSortChar
-//
-//  Created by Kian Davoudi-Rad on 2017-09-22.
-//  Copyright © 2017 Kian. All rights reserved.
-//
+#include "stdio.h"
 
-#import <Foundation/Foundation.h>
-
-int main(int argc, const char * argv[]) {
-    @autoreleasepool {
-        // insert code here...
-        NSLog(@"Hello, World!");
+void bubbleSortChar( char str []){
+    // find Lend of Char
+    int l =0;
+    char *prtLen = str;
+    while (*prtLen != '\0')
+    {
+        l++;
+        prtLen++;
     }
+
+
+for (int j =0; j<l; j++){
+    // setTwoPointers n and n+1
+    char *prtN = str;
+    char* prtNext = prtN+1;
+    char *prtEndOfCharArray = prtN + l;
+
+    for (int i =0; i<l; i++){
+        // repeat this process for all memeber of the array
+        while (prtNext<prtEndOfCharArray)
+        {
+
+             if (*prtN>*prtNext)
+            {
+                char temp = *prtN;
+                *prtN = *prtNext;
+                *prtNext = temp;
+
+            }
+        //    printf("%s\n",str);
+            prtN++;
+            prtNext++;
+
+
+
+        }
+
+    }
+}
+    printf("%s\n",str);
+
+}
+
+int main() {
+
+    char str []= "hooplah";
+    bubbleSortChar(str);
     return 0;
 }
