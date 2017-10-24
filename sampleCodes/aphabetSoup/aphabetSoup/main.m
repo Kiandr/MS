a19968fbc95bf1ae44df37ec4ead58c3b3d8b045
Kian Davoudi-Rad <davoudi.rk@gmail.com>
2017-10-24 09:56:30 -0700

    adding bullble sort.

---
+++ sampleCodes/aphabetSoup/aphabetSoup/main.m
@@ -0,0 +1,42 @@
+//
+//  main.m
+//  aphabetSoup
+//
+//  Created by Kian Davoudi-Rad on 2017-10-24.
+//  Copyright © 2017 Kian. All rights reserved.
+//
+
+
+#include <stdio.h>
+#include "stdbool.h"
+
+
+void bubbleSortArray(char *str){
+
+}
+
+
+void AlphabetSoup(char * str) {
+
+    bubbleSortArray(str);
+
+
+
+
+
+    printf("%s", str);
+
+}
+
+int main(void) {
+
+    char inpout [] = "ACBD";
+
+
+
+    // keep this function call here
+    AlphabetSoup(inpout);
+
+    return 0;
+
+}
