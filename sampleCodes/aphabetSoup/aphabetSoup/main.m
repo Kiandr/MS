56f2298b4309403728e8ad82caf2283af461ae07
Kian Davoudi-Rad <davoudi.rk@gmail.com>
2017-10-24 10:36:51 -0700

    added the same code from the previous bubble sort exercise

--- sampleCodes/aphabetSoup/aphabetSoup/main.m
+++ sampleCodes/aphabetSoup/aphabetSoup/main.m
@@ -1,42 +1,53 @@
-//
-//  main.m
-//  aphabetSoup
-//
-//  Created by Kian Davoudi-Rad on 2017-10-24.
-//  Copyright © 2017 Kian. All rights reserved.
-//
-
-
 #include <stdio.h>
 #include "stdbool.h"
 
-
-void bubbleSortArray(char *str){
-
-}
-
-
 void AlphabetSoup(char * str) {
+    // find Lend of Char
+    int l =0;
+    char *prtLen = str;
+    while (*prtLen != '\0')
+    {
+        l++;
+        prtLen++;
+    }
 
-    bubbleSortArray(str);
 
+    for (int j =0; j<l; j++){
+        // setTwoPointers n and n+1
+        char *prtN = str;
+        char* prtNext = prtN+1;
+        char *prtEndOfCharArray = prtN + l;
 
+        for (int i =0; i<l; i++){
+            // repeat this process for all memeber of the array
+            while (prtNext<prtEndOfCharArray)
+            {
 
+                if (*prtN>*prtNext)
+                {
+                    char temp = *prtN;
+                    *prtN = *prtNext;
+                    *prtNext = temp;
 
+                }
+                //    printf("%s\n",str);
+                prtN++;
+                prtNext++;
+
+            }
+
+        }
+    }
 
     printf("%s", str);
 
 }
 
 int main(void) {
-
-    char inpout [] = "ACBD";
-
-
-
-    // keep this function call here
+    char inpout [] = "coderbyte";
     AlphabetSoup(inpout);
 
-    return 0;
 
+    
+    return 0;
 }
