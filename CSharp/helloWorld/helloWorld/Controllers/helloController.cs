1e27a2df2445a8fd6777dfc9bef53836bdd4aea0
kiandr <davoudi.rk@gmail.com>
2018-01-25 15:48:17 -0800

    adding last effort for c#

---
+++ CSharp/helloWorld/helloWorld/Controllers/helloController.cs
@@ -0,0 +1,17 @@
+﻿
+using helloWorld.BusinessRepository;
+using System;
+
+namespace helloWorld.Controllers
+
+{
+    public class helloController: StringManipulation
+    {
+        public override void helloPrint() {
+            Console.Write("Will implement that function soon");
+        }
+
+
+
+    }
+}
---
+++ CSharp/helloWorld/helloWorld/Program.cs
@@ -0,0 +1,16 @@
+﻿using helloKian;
+using System;
+
+namespace helloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var newClass = new firstClass();
+            string input = Console.ReadLine();
+            Console.WriteLine(newClass.getValue()+input);
+        }
+    }
+}
