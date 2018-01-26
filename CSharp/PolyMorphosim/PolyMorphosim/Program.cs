f86f7a38280ae8937707f0e764b1e569a420c38e
kiandr <davoudi.rk@gmail.com>
2018-01-25 16:52:28 -0800

    adding polymorphosim

---
+++ CSharp/PolyMorphosim/PolyMorphosim/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PolyMorphosim;
+
+namespace PolyMorphosim
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var InstanceB = new drivedClassB(10,10,10);
+
+            InstanceB.printVal();
+            var InstanceA = new drivedClassA(10, 10, 10);
+   
+            InstanceA.printVal();
+
+
+
+        }
+    }
+}
---
+++ CSharp/PolyMorphosim/PolyMorphosim/drivedClass.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PolyMorphosim
+{
+    public class drivedClassA : baseClass
+    {
+        public drivedClassA(int weight, int deapth, int size) : base(weight, deapth, size)
+        {
+        }
+
+        public override void printVal()
+        {
+            //base.printVal();
+            Console.WriteLine("Derived Class A is" + getOz());
+        }
+    }
+
+
+    public class drivedClassB : baseClass
+    {
+        public drivedClassB(int weight, int deapth, int size) : base(weight, deapth, size)
+        {
+        }
+        public override void printVal()
+        {
+            //base.printVal();
+            Console.WriteLine("Derived Class B is" + getOz());
+        }
+    }
+}
