b27b9e8db2ee467d25770b211e2a0c9f9d1d59dc
kiandr <davoudi.rk@gmail.com>
2018-01-29 15:19:02 -0800

    Added linked list int generic format!

---
+++ Languages/CSharp/generic/Generic/Generic/Generic.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Generic
+{
+    public class linkedList<T>
+    {
+
+        public class node {
+            public T data { get; set; }
+            public node next { get; set; }
+            public node(T incomingData) {
+                data = incomingData;
+                next = null;
+            }
+        }
+        public node header;
+        public void addNewNode(node newNode)
+        {
+
+            if (header == null)
+            {
+                header = new node(newNode.data);
+            }
+            else
+            {
+                node prtH = header;
+                while (prtH.next != null)
+                    prtH = prtH.next;
+                prtH.next = new node(newNode.data);
+            }
+        }
+        public void printList() {
+            node prtH = header;
+            if (prtH == null)
+                Console.WriteLine("Header is empty!");
+            else {
+                while (prtH != null)
+                {
+                    Console.WriteLine(prtH.data);
+                    prtH = prtH.next;
+                }
+            }
+        }
+    }
+}
---
+++ Languages/CSharp/generic/Generic/Generic/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var list = new linkedList<int>();
+            for (int i = 10; i < 20; i++)
+                list.addNewNode(new linkedList<int>.node(i));
+            list.printList();
+        }
+    }
+}
