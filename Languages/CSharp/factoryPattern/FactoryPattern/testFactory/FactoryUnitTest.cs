703f75d0a5f48ef310bb00a62cbd1076630036c7
kiandr <davoudi.rk@gmail.com>
2018-01-28 16:17:25 -0800

    Renamed Some strings

---
+++ Languages/CSharp/factoryPattern/FactoryPattern/testFactory/FactoryUnitTest.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using FactoryPattern;
+
+namespace testFactory
+{
+    [TestClass]
+    public class FactoryUnitTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            factoryImplementation factory = new factoryImplementation();
+            var a = factory.printSavingAccountBalace("CITI-xx:xx");
+            var b = factory.printSavingAccountBalace("NATIONAL-xx:xx");
+            Assert.AreNotEqual(a.returnSavingInCad(),b.returnSavingInCad());
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            factoryImplementation factory = new factoryImplementation();
+            var a = factory.printSavingAccountBalace("CITI-xx:xx");
+            var b = factory.printSavingAccountBalace("CITI-xx:xx");
+            Assert.AreEqual(a.returnSavingInCad(), b.returnSavingInCad());
+        }
+    }
+}
