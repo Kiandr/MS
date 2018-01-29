65bf7577cc3a03bda87512e107a16fb6da3d8412
kiandr <davoudi.rk@gmail.com>
2018-01-28 16:15:12 -0800

    Factory Pattern

---
+++ Languages/CSharp/factoryPattern/FactoryPattern/FactoryPattern/Factory.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactoryPattern
+{
+    public interface ISavingAccount {
+        /// <summary>
+        /// This prints remaining of saving accout 
+        /// </summary>
+        /// <param name="accountName"> string containing account prefix - number </param>
+       string PrintSaving();
+
+    }
+
+
+    /// <summary>
+    /// This is Citi Bank Class for saving accounts 
+    /// </summary>
+    public class CitiBank : ISavingAccount
+    {
+
+        public string  PrintSaving()
+        {
+            var builder = new StringBuilder(); 
+            builder.Append("Remaining of Saving Account of Citi Bank is xx CAD");
+            return builder.ToString();
+        }
+    }
+
+    /// <summary>
+    /// This is National Class for saving account
+    /// </summary>
+    public class NationalBank : ISavingAccount
+    {
+        public string PrintSaving()
+        {
+
+            var builder = new StringBuilder();
+            builder.Append("Remaining of Saving Account of National Bank is xx CAD");
+            return builder.ToString();
+        }
+    }
+
+    /// <summary>
+    /// This is the contract that itnerface dictates for factory implementation 
+    /// </summary>
+    public interface IFactory {
+        ISavingAccount printSavingAccountBalace(string accountName);
+    }
+
+    /// <summary>
+    /// This is factory implementation
+    /// </summary>
+    public class factoryImplementation : IFactory
+    {
+        public ISavingAccount printSavingAccountBalace(string accountName)
+        {
+
+            if (accountName.Contains("CITI"))
+                return new CitiBank();
+            else if (accountName.Contains("NATIONAL"))
+                return new NationalBank();
+
+            else throw new Exception("The account you entered does not math with data base! Try National or Citi Prefix");
+
+        }
+    }
+}
---
+++ Languages/CSharp/factoryPattern/FactoryPattern/FactoryPattern/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactoryPattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            factoryImplementation factory = new factoryImplementation();
+            var a = factory.printSavingAccountBalace("CITI-xx:xx");
+            var b = factory.printSavingAccountBalace("NATIONAL-xx:xx");
+            Console.WriteLine(a.PrintSaving());
+            Console.WriteLine(b.PrintSaving());
+
+        }
+    }
+}
