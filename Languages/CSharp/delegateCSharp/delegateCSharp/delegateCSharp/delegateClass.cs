c01b60847b5aa927ac8003ee7a50aefca6655488
kiandr <davoudi.rk@gmail.com>
2018-02-21 19:54:30 -0800

    adding design patter

---
+++ Languages/CSharp/delegateCSharp/delegateCSharp/delegateCSharp/delegateClass.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace delegateCSharp
+{
+    public class delegateClass
+    {
+        
+        // public member 
+        public void DoWork() {
+            Console.WriteLine("Do work function was called");
+        }
+
+        public  delegate void DoWorkDelegate();
+    }
+}
+    /*
+    The goal of this class is to build a delegate and test it. 
+     
+     */
