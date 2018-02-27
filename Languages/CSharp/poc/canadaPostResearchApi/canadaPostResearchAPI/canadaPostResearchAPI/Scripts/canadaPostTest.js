210ce56f9243c968b70a22a33f0d67f28882112b
KianD.Rad <info@kian.co>
2018-02-26 17:07:30 -0800

    POC how to open camera on firefox. There is a lot of effort to make it work on all devices at the same time.

--- Languages/CSharp/poc/canadaPostResearchApi/canadaPostResearchAPI/canadaPostResearchAPI/Scripts/canadaPostTest.js
+++ Languages/CSharp/poc/canadaPostResearchApi/canadaPostResearchAPI/canadaPostResearchAPI/Scripts/canadaPostTest.js
@@ -126,3 +126,68 @@ function AddressComplete_Interactive_Find_v2_10End(response) {
         }
     }
 }
+
+
+
+var webCamService = { start: "startMethod", stop: "stopMethod", successCallback: "successCallBackMethod", errorCallback:"failedCallBackMethod" , constrainsForDevice: "constrainMethod"};
+
+webCamService.errorCallback = function (e) { console.log(e); }
+webCamService.constrainsForDevice = function () {
+    return {
+        video: true
+    }; }
+webCamService.successCallback = function (stream) {
+
+    debugger;
+    var video = document.querySelector("cameraTag");
+        video.srcObject = stream;
+        video.onloadedmetadata = function (e) {
+            // Do something with the video here.
+            video.play();
+        };
+};
+webCamService.start = function () { };
+
+
+
+
+
+
+var video = document.querySelector('video');
+    var canvas = document.querySelector('canvas');
+    var ctx = canvas.getContext('2d');
+    var image = document.querySelector('img');
+    navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia;
+
+    window.URL = window.URL || window.webkitURL;
+
+
+    function snapshot() {
+        var cw = video.clientWidth;
+        var ch = video.clientHeight;
+        ctx.drawImage(video, 0, 0, cw, ch, 0, 0, cw / 2, ch / 3);
+        image.src = canvas.toDataURL();
+        image.height = ch;
+        image.width = cw;
+    }
+
+    video.addEventListener('click', snapshot, false);
+
+    if (navigator.getUserMedia) {
+        navigator.getUserMedia({ video: true, audio: true },
+            function (stream) {
+                video.src = window.URL.createObjectURL(stream);
+                video.onloadedmetadata = function (e) {
+                    video.play();
+                };
+            },
+            function (err) {
+                console.log("The following error occured: " + err.name);
+            }
+        );
+    } else {
+        console.log("getUserMedia not supported");
+    }
+
+
+
